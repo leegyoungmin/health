86a2616f421d469795b0d8d041496ba763d54446
leegyoungmin <52390923+leegyoungmin@users.noreply.github.com>
2021-06-30 18:50:59 +0900

    User_data Version : Beta2

--- app/src/main/java/com/Health/health/Member/membertab.java
+++ app/src/main/java/com/Health/health/Member/membertab.java
@@ -1,52 +1,62 @@
 package com.Health.health.Member;
 
+import android.content.Context;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.threeten.bp.DayOfWeek;
 import org.threeten.bp.LocalDate;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.Health.health.R;
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
-public class membertab extends AppCompatActivity implements View.OnClickListener{
-    static final String[] LIST_MY={"LIST1","LIST2","LIST3"};
-    static final String[] LIST_CHAT={"CHAT1","CHAT2","CHAT3"};
-    String date;
-    MaterialCalendarView materialCalendarView;
-    CalendarDay calendar;
+import java.lang.ref.Reference;
+import java.util.HashMap;
 
+public class membertab extends AppCompatActivity{
+    String WEIGHT = new String();
+    String FAT=new String();
+    String MUSCLE=new String();
+    EditText weighttext,fattext,muscletext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_membertab);
+        setContentView(R.layout.activity_tab);
 
-
-        ActionBar actionBar=getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(true);
-        actionBar.setTitle("신체 변화");
         changeView(0);
 
-        TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabhost);
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                Log.e("tab_position", String.valueOf(tab.getPosition()));
-                String title = tab.getText().toString();
-                actionBar.setTitle(title);
-                actionBar.setDisplayShowHomeEnabled(true);
-                actionBar.setDisplayHomeAsUpEnabled(true);
-                int pos=tab.getPosition();
-                changeView(pos);
+                changeView(tab.getPosition());
             }
 
             @Override
@@ -61,49 +71,92 @@ public class membertab extends AppCompatActivity implements View.OnClickListener
         });
     }
     private void changeView(int index){
-        ListView listView1 = (ListView)findViewById(R.id.listview1);
-        ListView listView2 = (ListView)findViewById(R.id.listview2);
-        TextView textView = (TextView)findViewById(R.id.textView);
-        ScrollView scrollView1 =(ScrollView)findViewById(R.id.scrollView1);
+        LayoutInflater inflater =(LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
+        FrameLayout frameLayout = (FrameLayout)findViewById(R.id.framelayout1);
+        if(frameLayout.getChildCount()>0){
+            frameLayout.removeViewAt(0);
+        }
+
+        View view=null;
         switch (index){
             case 0:
-                ArrayAdapter adapter1 = new ArrayAdapter(this, android.R.layout.simple_list_item_1,LIST_MY);
-                listView1.setAdapter(adapter1);
-                listView1.setVisibility(View.VISIBLE);
-                listView2.setVisibility(View.GONE);
-                scrollView1.setVisibility(View.GONE);
-
                 break;
             case 1:
-                ArrayAdapter adapter2 = new ArrayAdapter(this, android.R.layout.simple_list_item_1,LIST_CHAT);
-                listView2.setAdapter(adapter2);
-                listView2.setVisibility(View.VISIBLE);
-                listView1.setVisibility(View.GONE);
-                scrollView1.setVisibility(View.GONE);
-
                 break;
             case 2:
-                int mYear=LocalDate.now().getYear();
-                int mMonth = LocalDate.now().getMonth().getValue();
-                int mDay=LocalDate.now().getDayOfMonth();
-                listView1.setVisibility(View.GONE);
-                listView2.setVisibility(View.GONE);
-                scrollView1.setVisibility(View.VISIBLE);
-                materialCalendarView=(MaterialCalendarView) scrollView1.findViewById(R.id.calendar);
-                materialCalendarView.state().edit()
+                view = inflater.inflate(R.layout.list,frameLayout,false);
+
+                MaterialCalendarView calendarView=view.findViewById(R.id.calendar);
+                calendarView.state().edit()
+                        .setFirstDayOfWeek(DayOfWeek.SUNDAY)
+                        .setMaximumDate(CalendarDay.from(LocalDate.now().getYear(),LocalDate.now().getMonth().getValue(),LocalDate.now().getDayOfMonth()))
                         .setCalendarDisplayMode(CalendarMode.WEEKS)
-                        .setMaximumDate(CalendarDay.from(mYear,mMonth,mDay)).commit();
-                break;
+                        .commit();
+                weighttext=view.findViewById(R.id.inputtextedit1);
+                fattext=view.findViewById(R.id.inputtextedit2);
+                muscletext=view.findViewById(R.id.inputtextedit3);
 
+                calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+                    @Override
+                    public void onDateSelected(@NonNull MaterialCalendarView widget, CalendarDay date, boolean selected) {
+                        if (selected==false){
+                            weighttext.setClickable(false);
+                            fattext.setClickable(false);
+                            muscletext.setClickable(false);
+                            Toast.makeText(membertab.this,"입력할 날짜를 선택해주세요.",Toast.LENGTH_SHORT).show();
+                        }
+                        int mYear = date.getYear();
+                        int mMonth = date.getMonth();
+                        int mDay = date.getDay();
+                    }
+                });
+
+                weighttext.addTextChangedListener(textWatcher);
+                fattext.addTextChangedListener(textWatcher);
+                muscletext.addTextChangedListener(textWatcher);
+
+                Log.e("WEIGHT",WEIGHT);
+                Log.e("FAT",FAT);
+                Log.e("MUSCLE",MUSCLE);
+        }
+        if(view !=null){
+            frameLayout.addView(view);
         }
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-
+    private final TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
         }
-    }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            WEIGHT = weighttext.getText().toString();
+            FAT = fattext.getText().toString();
+            MUSCLE = muscletext.getText().toString();
+
+            Log.e("WEIGHT", WEIGHT);
+            Log.e("FAT", FAT);
+            Log.e("MUSCLE", MUSCLE);
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            WEIGHT=weighttext.getText().toString();
+            FAT=fattext.getText().toString();
+            MUSCLE=muscletext.getText().toString();
+            HashMap hashMap = new HashMap();
+            hashMap.put("weight",WEIGHT);
+            hashMap.put("fat",FAT);
+            hashMap.put("muscle",MUSCLE);
+
+            String uid=FirebaseAuth.getInstance().getCurrentUser().getUid();
+            FirebaseDatabase database =FirebaseDatabase.getInstance();
+            DatabaseReference reference = database.getReference("Users");
+            reference.child(uid).child("User_data").setValue(hashMap);
+        }
+    };
+
 }
---
+++ app/src/main/java/com/Health/health/Member/myInflater.java
@@ -0,0 +1,8 @@
+package com.Health.health.Member;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+
+public class myInflater {
+
+}
