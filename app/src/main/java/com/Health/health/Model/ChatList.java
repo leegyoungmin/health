c8d2ac0878c2173a9b7b6ba6473063c772858532
LeeKwanHyeong <asd95101@naver.com>
2021-07-06 14:17:42 +0900

    2021-07-01 17:14

---
+++ app/src/main/java/com/Health/health/Model/ChatList.java
@@ -0,0 +1,22 @@
+package com.Health.health.Model;
+
+public class ChatList {
+
+    private String id;
+
+    public ChatList() {
+    }
+
+
+    public ChatList(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
